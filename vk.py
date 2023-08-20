c1ebc106e0619baada252cc2db715b9ac47cb07e
Yaroslav Voroshilov <frqhero@gmail.com>
2023-08-20 20:47:39 +0600

    Add monitoring for vk bot

--- vk.py
+++ vk.py
@@ -1,16 +1,32 @@
+import logging
 import os
 import random
+import traceback
 
+import telegram
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
 import vk_api as vk
 
 from run import detect_intent_texts
 
+
 load_dotenv()
 
 
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
 def echo(event, vk_api):
+    0 / 0
     is_fallback, response_text = detect_intent_texts(
         project_id, event.user_id, event.text, 'RU'
     )
@@ -28,6 +44,18 @@ if __name__ == '__main__':
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    tg_chat_id = os.getenv('TG_CHAT_ID_SEND_ERRORS_TO')
+    bot = telegram.Bot(token=telegram_token)
+    bot.logger.addHandler(TelegramLogsHandler(bot, tg_chat_id))
+    bot.logger.warning('Dialogflow bot via vk has been started')
+
+    while 1:
+        try:
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    echo(event, vk_api)
+        except Exception:
+            exception_data = traceback.format_exc().splitlines()
+            bot.logger.warning(exception_data[-1])
